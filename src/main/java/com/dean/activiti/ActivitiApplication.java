45e3713dda6d694b9c6f2fa023f3f102bb26b187
zhangchuqiang <zhangchuqiang119@163.com>
2018-11-10 16:25:50 +0800

    first commit

---
+++ src/main/java/com/dean/activiti/ActivitiApplication.java
@@ -0,0 +1,17 @@
+package com.dean.activiti;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ActivitiApplication {
+
+
+    public static void main(String[] args){
+
+        SpringApplication.run(ActivitiApplication.class);
+
+    }
+
+}
